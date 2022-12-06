e250e0238fc3b65672d72cccabbab7e5cf234781
Radosław Milczarek <116893317+RadoRadoslaw@users.noreply.github.com>
2022-12-06 12:32:12 +0100

    bat

--- Game/js/entities/bat.js
+++ Game/js/entities/bat.js
@@ -4,72 +4,64 @@ import { random } from "../logic.js";
 import { lifebar } from "../lifebar.js";
 
 export class bat extends Model {
-	constructor(x, y) {
-		super("bat", "images/bat.jpg", x, y, 82, 82);
-		this.life=100;
-		let bar = new lifebar(this.x, this.y);
-			game.objects.push(bar);
-		bar.x = 62
-		this.update = function () {
-			let cordX=random( -3, -3);
-			let cordY=random(-6,6);
-			this.x += cordX;
-			this.y += cordY;
-			
+  constructor(x, y) {
+    super("bat", "images/bat.jpg", x, y, 82, 82);
+    this.life = 100;
+    let bar = new lifebar(this.x, this.y);
+    game.objects.push(bar);
+    bar.x = 62;
+    this.update = function () {
+      let cordX = random(-3, -3);
+      let cordY = random(-6, 6);
+      this.x += cordX;
+      this.y += cordY;
 
-			if (this.life <= 0) {
-				this.destroy = true
-				bar.destroy = true
-				let wynik = this.get("points")
-				wynik.wynik += 1
-			}
+      if (this.life <= 0) {
+        this.destroy = true;
+        bar.destroy = true;
+        let wynik = this.get("points");
+        wynik.wynik += 1;
+      }
 
-			// this.collide("bat", function (bat) {
-			// 	this.destroy = true
-			// 	bat.life -= 40
-			// });
+    //   this.collide("bat", function (bat) {
+    //     this.destroy = true;
+    //     bat.life -= 40;
+    //   });
 
-			// let player = this.collide("player");
-			// if (player) {
-			// 	player.image="images/dead.jpg"
-			// 	player.w=264;
-			// 	player.h=264;
-			// 	player.stop = true
-		
-			// }
+    //   let player = this.collide("player");
+    //   if (player) {
+    //     player.image = "images/dead.jpg";
+    //     player.w = 264;
+    //     player.h = 264;
+    //     player.stop = true;
+    //   }
 
-			
-			// this.collide("player",function (player, x, y) {
-			// 	player.image="images/dead.jpg"
-			// 	player.w=264;
-			// 	player.h=264;
-			// 	player.stop = true
-			// });
-				
+      this.collide("player", function (player, x, y) {
+        player.image = "images/dead.jpg";
+        player.w = 264;
+        player.h = 264;
+        player.stop = true;
+      });
 
-			if (this.x >= window.innerWidth) {
-				this.x = -2 ;
-			}
-			if (this.y >= window.innerHeight) {
-				this.y = -2 ;
-			}
-			if (this.x <= 0) {
-				this.x = window.innerWidth;
-			}
-			if (this.y <= 0) {
-				this.y = 0;
-			}
-				bar.x = this.x
-				bar.y = this.y - 10
-				bar.w = 62 / 100 * this.life
-			if (this.life <= 0) {
-				this.destroy = true
-				bar.destroy = true
-			}
-			
-				
-	
-		}
-	}
-
+      if (this.x >= window.innerWidth) {
+        this.x = -2;
+      }
+      if (this.y >= window.innerHeight) {
+        this.y = -2;
+      }
+      if (this.x <= 0) {
+        this.x = window.innerWidth;
+      }
+      if (this.y <= 0) {
+        this.y = 0;
+      }
+      bar.x = this.x;
+      bar.y = this.y - 10;
+      bar.w = (62 / 100) * this.life;
+      if (this.life <= 0) {
+        this.destroy = true;
+        bar.destroy = true;
+      }
+    };
+  }
 }
--- Game/js/entities/player.js
+++ Game/js/entities/player.js
@@ -8,199 +8,174 @@ import { level3 } from "../levels/level3.js";
 import { level4 } from "../levels/level4.js";
 
 export class Player extends Model {
-	constructor(x, y) {
-		// Pierwszy parametr to klasa w HTML'u, drugi to adres do zdjęcia, trzeci parametr to oś X, czwarty to oś Y, piąty to szerokość, szósty to wysokość
-		// super(className,image,x,y,width,height)
-		super("player", "images/player.png", x, y, 42, 42);
-		const that = this;
-		this.canShot = 0;
-		this.canJump = false;
-		this.speedY = 0;
-		this.speedX = 0;
-		this.stop = false;
-		this.life = 1;
-		this.update = function () {
-			// >>>>>
+  constructor(x, y) {
+    // Pierwszy parametr to klasa w HTML'u, drugi to adres do zdjęcia, trzeci parametr to oś X, czwarty to oś Y, piąty to szerokość, szósty to wysokość
+    // super(className,image,x,y,width,height)
+    super("player", "images/player.png", x, y, 42, 42);
+    const that = this;
+    this.canShot = 0;
+    this.canJump = false;
+    this.speedY = 0;
+    this.speedX = 0;
+    this.stop = false;
+    this.life = 1;
+    this.update = function () {
+      // >>>>>
 
-			
-			this.timer -= 1;
+      this.timer -= 1;
 
-			if (this.timer == 60 * 3) {
-				game.objects.push(new Zombie(this.x, this.y + 6,));
-			}
+      if (this.timer == 60 * 3) {
+        game.objects.push(new Zombie(this.x, this.y + 6));
+      }
 
-			if (this.life <= 0) {
-				this.destroy = true;
-			}
-			if (this.destroy == true) {
-				this.destroy = false;
-				this.image = "images/dead.jpg"
-				this.w = 264;
-				this.h = 264;
-				this.stop = true;
-			}
+      if (this.life <= 0) {
+        this.destroy = true;
+      }
+      if (this.destroy == true) {
+        this.destroy = false;
+        this.image = "images/dead.jpg";
+        this.w = 264;
+        this.h = 264;
+        this.stop = true;
+      }
 
+      // Prędkość x wytracanie
+      if (this.canJump == false) {
+        this.speedX *= 0.99;
+      } else {
+        this.speedX *= 0.9;
+      }
 
-			// Prędkość x wytracanie
-			if(this.canJump==false){
-	
-				this.speedX *= .99;
-			
-			}else {
-				this.speedX *=.9;
-			}
-			
-			
+      if (this.stop == false && this.canJump == true) {
+        if (keyboard.left) {
+          // Dodawanie prędkości x
+          this.speedX += -0.5;
+          // Limit prędkości x
+          if (this.speedX <= -4) {
+            this.speedX = -4;
+          }
 
-			if (this.stop == false && this.canJump == true) {
-				if (keyboard.left) {
-					// Dodawanie prędkości x
-					this.speedX += -0.5;
-					// Limit prędkości x
-					if (this.speedX <= -4) {
-						this.speedX = -4;
-					} 
+          this.rotate = -1;
+        }
+        if (keyboard.right && this.canJump == true) {
+          this.speedX += 0.5;
+          if (this.speedX >= 4) {
+            this.speedX = 4;
+          }
+          this.rotate = 1;
+        }
+        if (keyboard.up == true && this.canJump == true) {
+          sfx.jump.currentTime = 0;
+          sfx.jump.play();
+          this.speedY = -17;
+          this.canJump = false;
+        }
+      }
 
-			
-				
-					this.rotate = -1;
+      // Dodawanie prędkości x do samego koordynatu X
+      this.x += this.speedX;
 
-				}
-				if (keyboard.right && this.canJump == true) {
-					this.speedX += .5;
-					if (this.speedX >= 4) {
-						this.speedX = 4;
-					}
-					this.rotate = 1;
-				}
-				if (keyboard.up == true && this.canJump == true) {
-					sfx.jump.currentTime = 0;
-					sfx.jump.play();
-					this.speedY = -17;
-					this.canJump = false;
+      if (this.x < 0) {
+        this.x = 0;
+      }
 
-				}
+      if (this.x >= game.width - this.w && game.level != 2 && game.level != 3) {
+        level2();
+      }
+      if (this.x <= 1 && game.level != 1) {
+        level1(game.width - this.w - 1, game.height);
+      }
 
-			}
+      if (this.x >= game.width - this.w && game.level != 1 && game.level != 3) {
+        this.x = game.width - this.w;
+      }
 
-			// Dodawanie prędkości x do samego koordynatu X
-			this.x += this.speedX;
+      if (this.x >= game.width - 90 && game.level != 1 && game.level != 2) {
+        this.x = game.width - 90;
+      }
+      if (this.x <= 41 && game.level != 1 && game.level != 2) {
+        this.x = 41;
+      }
 
-			if (this.x < 0) {
-				this.x = 0;
-			}
+      // if (this.x >= window.innerWidth - this.w) {
+      // 	this.x = window.innerWidth - this.w
+      // }
 
-			if (this.x >= game.width - this.w && game.level != 2 && game.level != 3) {
-				level2();
-			}
-			if (this.x <= 1 && game.level != 1) {
-				level1(game.width - this.w -1, game.height );
-			}
+      if (keyboard.space == true && this.canShot <= 0) {
+        game.objects.push(new Bullet(this.x, (this.y += 12), this.rotate));
+        sfx.shot.currentTime = 0;
+        sfx.shot.play();
+        keyboard.space = false;
+        this.canShot = 60;
+      }
+      if (this.canShot > 0) {
+        this.canShot -= 1;
+      }
 
-			if (this.x >= game.width - this.w && game.level != 1 && game.level != 3) {
-				this.x = game.width - this.w
-			}
+      if (this.canJump == false) {
+        this.speedY *= 0.98;
+        this.y += this.speedY;
+        this.y += 6;
+      }
 
-			if (this.x >= game.width - 90 && game.level != 1 && game.level != 2) {
-				this.x = game.width - 90
-			}
-			if (this.x <= 41 && game.level != 1 && game.level != 2) {
-				this.x = 41
-			}
+      that.canJump = false;
 
-			// if (this.x >= window.innerWidth - this.w) {
-			// 	this.x = window.innerWidth - this.w
-			// }
+      let trawka = this.get("trawka");
+      if (this.y >= trawka.y - this.h) {
+        this.y = trawka.y - this.h;
+        this.canJump = true;
+      }
 
+      this.collide("box", function (box, x, y) {
+        if (x) {
+          this.x = this.old.x;
+          this.canJump = true;
+          if (box.x + box.w < this.x) {
+            this.speedX += 4;
+          }
+          if (box.x > this.x + this.w) {
+            this.speedX -= 4;
+          }
+          this.canJump = false;
+        }
+        if (y) {
+          this.y = this.old.y;
+          this.canJump = true;
+          if (box.y + box.h < this.y) {
+            this.speedY += 2;
+            this.canJump = false;
+          }
+        }
+      });
 
-			if (keyboard.space == true && this.canShot <= 0) {
-				game.objects.push(new Bullet(this.x, this.y += 12, this.rotate));
-				sfx.shot.currentTime = 0;
-				sfx.shot.play();
-				keyboard.space = false;
-				this.canShot = 60;
+      this.collide("pipe", function (pipe, x, y) {
+        if (x) {
+          this.x = this.old.x;
+        }
+        if (y) {
+          this.y = this.old.y;
+          this.canJump = true;
+        }
 
-			}
-			if (this.canShot > 0) {
-				this.canShot -= 1;
+        if (keyboard.down == true) {
+          level3();
+          keyboard.down = false;
+        }
+      });
+      this.collide("box2", function (box2, x, y) {
+        if (x) {
+          this.x = this.old.x;
+        }
+        if (y) {
+          this.y = this.old.y;
+          this.canJump = true;
+        }
 
-
-			}
-
-
-			if (this.canJump == false) {
-				this.speedY *= .98;
-				this.y += this.speedY;
-				this.y += 6
-			}
-
-			that.canJump = false;
-
-			let trawka = this.get("trawka");
-			if (this.y >= trawka.y - this.h) {
-				this.y = trawka.y - this.h
-				this.canJump = true;
-			}
-
-			this.collide("box", function (box, x, y) {
-				if (x) {
-					this.x = this.old.x;
-					this.canJump = true;
-					if(box.x + box.w < this.x){
-						this.speedX +=4;
-					}
-					if(box.x > this.x + this.w){
-						this.speedX -=4;
-					}
-					this.canJump =false;
-				}
-				if (y) {
-					this.y = this.old.y;
-					this.canJump = true;
-					if (box.y + box.h < this.y) {
-						this.speedY += 2;
-						this.canJump = false;
-					}
-				
-				}
-			});
-
-
-
-
-
-			this.collide("pipe", function (pipe, x, y) {
-				if (x) {
-					this.x = this.old.x;
-				}
-				if (y) {
-					this.y = this.old.y;
-					this.canJump = true;
-				}
-
-				if (keyboard.down == true) {
-					level3();
-					keyboard.down = false;
-				}
-			});
-			this.collide("box2", function (box2, x, y) {
-				if (x) {
-					this.x = this.old.x;
-				}
-				if (y) {
-					this.y = this.old.y;
-					this.canJump = true;
-				}
-
-				if (keyboard.down == true) {
-					level4();
-					keyboard.down = false;
-				}
-			});
-
-
-
-		}
-
-	}
+        if (keyboard.down == true) {
+          level4();
+          keyboard.down = false;
+        }
+      });
+    };
+  }
 }
